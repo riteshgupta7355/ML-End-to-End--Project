1ce1db22a1e3edf5d312a420ae77bc864e710d35
Ritesh Gupta <riteshgupta7355@gmail.com>
2023-03-11 21:40:24 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-
+from src.logger import logging
 
 
 def error_message_detail(error,error_detail:sys):
@@ -22,7 +22,5 @@ class CustomException(Exception):
         return self.error_message   
 
 
-
-    
 
 
